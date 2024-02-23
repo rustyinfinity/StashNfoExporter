ca664765b38c89faad4ac41badcf25b0b16b52c2
PuSSy_C0NN0ISS3UR <PuSSy_C0NN0ISS3UR@proton.me>
2024-02-22 20:17:37 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_key = ''
+api_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJCb2JTYXBwNjkiLCJpYXQiOjE2NzA4ODI4NDMsInN1YiI6IkFQSUtleSJ9.5WcO_JvjLWVtMVHKWcYRG5xWPwKsLHmA2sgdViauzik'
 graphql_url = 'http://localhost:9999/graphql'
 organized = True
 parallel_workers = 10
