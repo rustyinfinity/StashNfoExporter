364a5ec5a9c4b69d2ae20f6f36b6bbaef378933c
rustyinfinity <115462641+rustyinfinity@users.noreply.github.com>
2024-02-28 01:54:47 +0530

    Fixes and Improvments
    Added Database to check whether files are being updated or not and update changed files only
    Fixed Studio bug
    Fixed tags saved as genre 
    Improved Performance

--- StashNfoExporter.py
+++ StashNfoExporter.py
@@ -1,3 +1,5 @@
+from genericpath import exists
+from json import loads
 import requests
 import config
 import log
@@ -5,6 +7,7 @@ import lxml.etree as etree
 import datetime
 import os
 import time
+import sqlite3
 
 HEADERS = {
     'Content-Type': 'application/json',
@@ -18,7 +21,7 @@ def make_graphql_request(query):
     try:
         json_query = {"query": query}
         response = requests.post(config.graphql_url, json=json_query, headers=HEADERS)
-        response.raise_for_status()  
+        response.raise_for_status()
         return response.json()
     except requests.exceptions.RequestException as e:
         log.error(f"Error making GraphQL request: {e}")
@@ -48,8 +51,45 @@ def process_scene(scene):
     sort_title_element = etree.SubElement(root, "sorttitle")
     sort_title_element.text = scene['title']
 
+    # User Rating
+    userrating_element = etree.SubElement(root, "userrating")
+    userrating_element.text = str(scene['rating100'])
+
+    # Plot
+    plot_element = etree.SubElement(root, "plot")
+    plot_element.text = scene['details']
+
+    # Studio Thumbnail (Logo)
+    thumb_studio_element = etree.SubElement(root, "thumb", attrib={"aspect": "clearlogo"})
+    if scene['studio'] is None:
+        thumb_studio_element.text = ""
+    else:
+        thumb_studio_element.text =  scene['studio']['image_path']
+
+    # Landscape Thumbnail
+    thumb_landscape_element = etree.SubElement(root, "thumb", attrib={"aspect": "landscape"})
+    thumb_landscape_element.text = scene['paths']['screenshot']
+
+    # Vertical Thumbnail
+    thumb_poster_element = etree.SubElement(root, "thumb", attrib={"aspect": "poster"})
+    thumb_poster_element.text = scene['paths']['screenshot']
+
+    # Fanart Thumbnail
+    fanart_element = etree.SubElement(root, "fanart")
+    fanart_thumb_element = etree.SubElement(fanart_element, "thumb")
+    fanart_thumb_element.text = scene['paths']['screenshot']
+
+    # MPAA Rating
+    mpaa_element = etree.SubElement(root, "mpaa")
+    mpaa_element.text = "XXX"
+
+    # Play Count
+    playcount_element = etree.SubElement(root, "playcount")
+    playcount_element.text = str(scene['play_count'])
+
     # Unique ID
     unique_id_element = etree.SubElement(root, "uniqueid")
+    unique_id_element.set("default", "true")
     unique_id_element.text = str(scene['id'])
 
     # Stash IDs
@@ -57,17 +97,22 @@ def process_scene(scene):
         stash_id_element = etree.SubElement(root, "uniqueid", type="stashdb")
         stash_id_element.text = str(stash_id['stash_id'])
 
-    # MPAA Rating
-    mpaa_element = etree.SubElement(root, "mpaa")
-    mpaa_element.text = "XXX"  # Adjust as per your requirements
+    # Tag
+    for tag in scene['tags']:
+        tag_element = etree.SubElement(root, "tag")
+        tag_element.text = tag['name']
 
-    # Play Count
-    playcount_element = etree.SubElement(root, "playcount")
-    playcount_element.text = str(scene['play_count'])
+    # Set
+    set_element = etree.SubElement(root, "set")
+    set_name_element = etree.SubElement(set_element, "name")
+    if scene['studio'] is None:
+        set_name_element.text = "No Studio"
+    else:
+        set_name_element.text = scene['studio']['name']
 
-    # Date Added
-    dateadded_element = etree.SubElement(root, "dateadded")
-    dateadded_element.text = str(scene['created_at'])
+    # Director
+    director_element = etree.SubElement(root, "director")
+    director_element.text = scene['director']
 
     # Premiered Date
     premiered_element = etree.SubElement(root, "premiered")
@@ -78,75 +123,18 @@ def process_scene(scene):
         year_element = etree.SubElement(root, "year")
         year_element.text = str(datetime.datetime.strptime(str(scene['date']),"%Y-%m-%d").year)
 
-    # User Rating
-    userrating_element = etree.SubElement(root, "userrating")
-    userrating_element.text = str(scene['rating100'])
-
-    # Plot
-    plot_element = etree.SubElement(root, "plot")
-    plot_element.text = scene['details']
-
-    # Outline
-    outline_element = etree.SubElement(root, "outline")
-    outline_element.text = scene['details']
-
     # Studio
-    studio = scene['studio']
     studio_element = etree.SubElement(root, "studio")
-    studio_element.text = studio['name']
-    studio_id_element = etree.SubElement(studio_element, "id")
-    studio_id_element.text = str(studio['id'])
-    studio_url_element = etree.SubElement(studio_element, "url")
-    studio_url_element.text = studio['url']
-
-    # Studio Thumbnail
-    thumb_studio_element = etree.SubElement(root, "thumb", attrib={"aspect": "clearlogo"})
-    thumb_studio_element.text = studio['image_path']
-
-    # Set
-    set_element = etree.SubElement(root, "set")
-    set_name_element = etree.SubElement(set_element, "name")
-    set_name_element.text = studio['name']
-    set_thumb_element = etree.SubElement(set_element, "thumb", attrib={"aspect": "poster"})
-    set_thumb_element.text = studio['image_path']
-
-    # Director
-    director_element = etree.SubElement(root, "director")
-    director_element.text = scene['director']
-
-    # Thumbnails
-    thumb_poster_element = etree.SubElement(root, "thumb", attrib={"aspect": "poster"})
-    thumb_poster_element.text = scene['paths']['screenshot']
-
-    thumb_landscape_element = etree.SubElement(root, "thumb", attrib={"aspect": "landscape"})
-    thumb_landscape_element.text = scene['paths']['screenshot']
-
-    fanart_element = etree.SubElement(root, "fanart")
-    fanart_thumb_element = etree.SubElement(fanart_element, "thumb")
-    fanart_thumb_element.text = scene['paths']['screenshot']
-
-    # Actors
-    for actor in scene['performers']:
-        actor_element = etree.SubElement(root, "actor")
-        actor_name_element = etree.SubElement(actor_element, "name")
-        actor_name_element.text = actor['name']
-        gender_element = etree.SubElement(actor_element, "gender")
-        gender_element.text = actor['gender']
-        image_element = etree.SubElement(actor_element, "thumb", attrib={"aspect": "poster"})
-        image_element.text = actor['image_path']
-        profile_element = etree.SubElement(actor_element, "profile")
-        profile_element.text = actor['url']
-
-    # Genre
-    for tag in scene['tags']:
-        tag_element = etree.SubElement(root, "tag")
-        tag_element.text = tag['name']
+    if scene['studio'] is None:
+         studio_element.text = ""
+    else:
+         studio_element.text = scene['studio']['name']
 
     # File Information
     for file_info in scene['files']:
         fileinfo_element = etree.SubElement(root, "fileinfo")
         streamdetails_element = etree.SubElement(fileinfo_element, "streamdetails")
-        
+
         # Video
         video_element = etree.SubElement(streamdetails_element, "video")
         video_codec_element = etree.SubElement(video_element, "codec")
@@ -165,6 +153,23 @@ def process_scene(scene):
         audio_codec_element = etree.SubElement(audio_element, "codec")
         audio_codec_element.text = file_info['audio_codec']
 
+
+    # Actors
+    order = 0
+    for actor in scene['performers']:
+        actor_element = etree.SubElement(root, "actor")
+        actor_name_element = etree.SubElement(actor_element, "name")
+        actor_name_element.text = actor['name']
+        order_element = etree.SubElement(actor_element, "order")
+        order_element.text = str(order)
+        image_element = etree.SubElement(actor_element, "thumb", attrib={"aspect": "poster"})
+        image_element.text = actor['image_path']
+        order += 1
+
+    # Date Added
+    dateadded_element = etree.SubElement(root, "dateadded")
+    dateadded_element.text = str(scene['created_at'])
+
     return root
 
 query = """
@@ -179,6 +184,7 @@ query = """
     organized
     play_count
     created_at
+    updated_at
     files {
       path
       video_codec
@@ -214,6 +220,18 @@ query = """
 
 START_TIME = time.time()
 
+conn = sqlite3.connect('updates_data.db')
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS scenes (
+                    id INTEGER PRIMARY KEY,
+                    title TEXT,
+                    last_updated_at TEXT
+                  )''')
+conn.commit()
+cursor.execute("SELECT id, last_updated_at FROM scenes")
+rows = cursor.fetchall()
+last_updated_dict = {row[0]: row[1] for row in rows}
+
 r = make_graphql_request(query)
 
 if r is not None:
@@ -222,17 +240,45 @@ if r is not None:
                            if (config.generate_nfo_for_files == 'Organized' and scene['organized']) or
                            (config.generate_nfo_for_files == 'Unorganized' and not scene['organized']) or
                            (config.generate_nfo_for_files == 'All')]
-        
-        
+
         for scene in organizedScenes:
-            root = process_scene(scene)
-            file_path = scene['files'][0]['path'] 
+            file_path = scene['files'][0]['path']
             file_name = os.path.basename(file_path)
             file_name_without_ext = os.path.splitext(file_name)[0]
             file_base_dir = os.path.dirname(file_path)
             nfo_file_name = f'{file_name_without_ext}.nfo'
+            # For testing only save path is my local  nfo_test folder !
             nfo_file_path = os.path.join(file_base_dir, nfo_file_name)
-            write_nfo(root, nfo_file_path)
+            if int(scene['id']) in last_updated_dict and scene['updated_at'] == last_updated_dict[int(scene['id'])] and os.path.exists(nfo_file_path):
+                 continue
+            else:
+                print("Changes Detected Adding to Database" +scene['title'] )
+                cursor.execute('''INSERT OR REPLACE INTO scenes (id, title, last_updated_at)
+                                VALUES (?, ?, ?)''', (scene['id'], scene['title'], scene['updated_at']))
+                conn.commit()
+                last_updated_dict[scene['id']] = scene['updated_at']
+        for scene in organizedScenes:
+            file_path = scene['files'][0]['path']
+            file_name = os.path.basename(file_path)
+            file_name_without_ext = os.path.splitext(file_name)[0]
+            file_base_dir = os.path.dirname(file_path)
+            nfo_file_name = f'{file_name_without_ext}.nfo'
+            # For testing only save path is my local  nfo_test folder !
+            nfo_file_path = os.path.join(file_base_dir, nfo_file_name)
+            if int(scene['id']) in last_updated_dict and scene['updated_at'] == last_updated_dict[int(scene['id'])] and os.path.exists(nfo_file_path):
+                 continue
+            else:
+                print("Changes Detected" ,scene['title'] )
+                root = process_scene(scene)
+                file_path = scene['files'][0]['path']
+                file_name = os.path.basename(file_path)
+                file_name_without_ext = os.path.splitext(file_name)[0]
+                file_base_dir = os.path.dirname(file_path)
+                nfo_file_name = f'{file_name_without_ext}.nfo'
+                # For testing only save path is my local  nfo_test folder !
+                # For Custom Path you can change base dir
+                nfo_file_path = os.path.join(file_base_dir, nfo_file_name)
+                write_nfo(root, nfo_file_path)
         log.debug("Execution time: {}s".format(round(time.time() - START_TIME, 5)))
     else:
         log.error("Please Check config -> make_graphql_request")
